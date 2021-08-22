463c021a91fdca5d660a0acac503e44270e7d568
hllneerk <hllneerk@gmail.com>
2021-08-22 13:00:46 +0900

    include mean std

--- check_overfit.py
+++ check_overfit.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     indicators = ['wkRSI','wkBB','wkATR','wkSMA']
     y_label = 'wkFixPips'
     indicator_start_label = 'wkSMA[0][0]'
+    pc_num = 12
 
     # load csv
     df = pd.read_csv(str(csv_path))
@@ -35,30 +36,25 @@ if __name__ == '__main__':
     is_train = ent_years <= 2019
     is_test = ~is_train
 
-
     '''
     training
     '''
     pc = []
-    for bs_i, buy_or_sell in enumerate([is_buy & is_train, (~is_buy) & is_train]):
-
-        if bs_i == 0:
-            bs = 'buy'
-        else:
-            bs = 'sell'
+    for bs, target_index in zip(['buy', 'sell'],[is_buy & is_train, (~is_buy) & is_train]):
 
         # pca to compress indicator value
-        X = np.array([df.iloc[i,st_indicator:].values for i in range(len(df)) if (buy_or_sell[i])])
-        X = np.array([(X[:,i]-np.mean(X[:,i]))/np.std(X[:,i]) for i in range(len(X[0]))]).T
-        pca = PCA(n_components=6)
+        X = np.array([df.iloc[i,st_indicator:].values for i in range(len(df)) if (target_index[i])]).astype(float)
+        MN, STD = np.mean(X, axis=0), np.std(X, axis=0)
+        X = np.array([(X[:,i]-MN[i])/STD[i] for i in range(len(X[0]))]).T
+        pca = PCA(n_components=pc_num)
         pca.fit(X)
         Xd = pca.transform(X)
-        print('Cumulative contribution rate from 1st to 6th PC:')
+        print('Cumulative contribution rate from 1st to {}th PC:'.format(pc_num))
         print(np.cumsum(pca.explained_variance_ratio_))
 
-        pc.append(pca.components_)
+        pc.append(np.concatenate([MN.reshape(1,-1),STD.reshape(1,-1),pca.components_],axis=0))
 
-        for xi in range(6):
+        for xi in range(pc_num):
 
             # sliding window
             w_gp_mean, w_gp_std, w_cent, w_num = [], [], [], []
@@ -66,11 +62,11 @@ if __name__ == '__main__':
             w_stride = (w_max-w_min)/50
             w_width = w_stride*2
             for w_st in np.arange(w_min,w_max,w_stride):
+                is_w = (Xd[:,xi] >= w_st) & (Xd[:,xi] < w_st+w_width)
                 w_cent.append(w_st + (w_width/2))
-                is_w = (Xd[:,xi] >= w_st) & (Xd[:,xi] < w_st+10)
                 w_num.append(is_w.sum())
-                w_gp_mean.append(np.mean(df[y_label].values[buy_or_sell][is_w]))
-                w_gp_std.append(np.std(df[y_label].values[buy_or_sell][is_w]))
+                w_gp_mean.append(np.mean(df[y_label].values[target_index][is_w]))
+                w_gp_std.append(np.std(df[y_label].values[target_index][is_w]))
             w_cent = np.array(w_cent)
             w_gp_mean = np.array(w_gp_mean)
             w_gp_std = np.array(w_gp_std)
@@ -82,7 +78,7 @@ if __name__ == '__main__':
             plt.figure()
             plt.scatter(
                 Xd[:,xi],
-                df[y_label].values[buy_or_sell],
+                df[y_label].values[target_index],
                 label='samples',
                 marker='+')
             plt.plot(w_cent,w_gp_mean*10,color='r',label=y_label+'_mean x 10')
@@ -99,64 +95,65 @@ if __name__ == '__main__':
             plt.savefig(fname)
 
     # save principal components
-    pc = np.array(pc).reshape(len(pc[0])*2,-1)
-    df_pc= pd.DataFrame(pc.T, 
-        columns = ['buy_pc{:02}'.format(num+1) for num in range(6)]+['sell_pc{:02}'.format(num+1) for num in range(6)],
+    pc_mat = np.array(pc).reshape(len(pc[1])*2,-1)
+    df_pc= pd.DataFrame(pc_mat.T, 
+        columns = ['buy_mean', 'buy_std'] + ['buy_pc{:02}'.format(num+1) for num in range(pc_num)] + \
+            ['sell_mean', 'sell_std'] + ['sell_pc{:02}'.format(num+1) for num in range(pc_num)],
         index = keys[st_indicator:])
     print('saving: {}'.format(str(out_pc_path)))
     df_pc.to_csv(str(out_pc_path))
 
     # define threshold
-    buy_pc_rm_th = [[10,17],[8,10],[8,19],[10,15],[9,15],[5,8]]
-    sell_pc_rm_th = [[18,36],[5,12],[3,20],[3,12],[20,20],[1.5,15]]
+    pc_rm_th = {
+        'buy': [[12,21],[9.5,11],[100,100],[100,100],[3.5,4.5],[5.5,7],
+                [3,4],[2.5,3],[3.5,4.5],[100,100],[2.5,3],[-3,-2]], 
+        'sell': [[27,50],[6,11],[7.5,11],[-7,-2.5],[3,5],[3,5],
+                [2,2.5],[-4,-3],[1,2],[1.5,2],[2.5,10],[-3.5,-2]]
+        }
 
     '''
     test
     '''
+    use_pc = {'buy':[4], 'sell':[0,4,5]}
     test_results, trading_results = [], []
-    for bs_i, buy_or_sell in enumerate([is_buy & is_test, (~is_buy) & is_test]):
-
-        if bs_i == 0:
-            bs = 'buy'
-        else:
-            bs = 'sell'
-
+    for bs, target_index in zip(['buy', 'sell'],[is_buy & is_test, (~is_buy) & is_test]):
+    # for bs, target_index in zip(['buy', 'sell'],[is_buy & is_train, (~is_buy) & is_train]):
+    
         # normalize data: actually the process should use all-period data
-        X = np.array([df.iloc[i,st_indicator:].values for i in range(len(df)) if (buy_or_sell[i])])
-        X = np.array([(X[:,i]-np.mean(X[:,i]))/np.std(X[:,i]) for i in range(len(X[0]))]).T
+        X = np.array([df.iloc[i,st_indicator:].values for i in range(len(df)) if (target_index[i])]).astype(float)
+        MN, STD = df_pc[bs+'_mean'].values, df_pc[bs+'_std'].values
+        X = np.array([(X[:,i]-MN[i])/STD[i] for i in range(len(X[0]))]).T
 
         # calculate pc values
-        PC_val = np.dot(X,pc[:6,:].T)
+        col_st = list(df_pc.keys()).index(bs+'_pc01')
+        PC_val = np.dot(X, df_pc.iloc[:,col_st:col_st + pc_num].values)
 
         # make thresholding flag
         rm_flag = []
         for i in range(len(PC_val)):
-            if bs == 'buy':
-                pc_rm_th = buy_pc_rm_th
-            elif bs == 'sell':
-                pc_rm_th = sell_pc_rm_th
             rm_tmp = False
-            for pc_rm_th_i in pc_rm_th:
-                if (PC_val[i,0] >= pc_rm_th_i[0]) & (PC_val[i,0] < pc_rm_th_i[1]):
+            for j in use_pc[bs]:
+                if (PC_val[i,j] >= pc_rm_th[bs][j][0]) & (PC_val[i,j] < pc_rm_th[bs][j][1]):
                     rm_tmp = True
             rm_flag.append(rm_tmp)        
         rm_flag = np.array(rm_flag)
 
         # results
-        FixPips_sum_before = df[y_label][buy_or_sell].sum()
-        FixPips_sum_after = df[y_label][buy_or_sell][~rm_flag].sum()
+        FixPips_sum_before = df[y_label][target_index].sum()
+        FixPips_sum_after = df[y_label][target_index][~rm_flag].sum()
         trading_results.append([rm_flag.sum(), len(rm_flag)])
         test_results.append([FixPips_sum_before,FixPips_sum_after])
 
     test_results = np.array(test_results)
     print('buy:[cancel_num, trading_num], sell:[cancel_num, trading_num]')
     print(trading_results)
+    print(test_results)
 
     plt.close('all')
     plt.figure()
-    plt.bar(['buy','buy w/ pc-th','sell','sell w/ pc-th'], [test_results[0,0],0,test_results[1,0],0],alpha=0.6)
-    # plt.bar(['buy_before','buy_after','sell_before','sell_after'], [0,test_results[0,1],0,test_results[1,1]],alpha=0.6)
-    plt.bar(['buy','buy w/ pc-th','sell','sell w/ pc-th'], [0,test_results[0,1],0,test_results[1,1]],alpha=0.6)
+    x = ['buy','buy w/ pc{}-th'.format(use_pc['buy']),'sell','sell w/ pc{}-th'.format(use_pc['sell'])]
+    plt.bar(x, [test_results[0,0],0,test_results[1,0],0],alpha=0.6)
+    plt.bar(x, [0,test_results[0,1],0,test_results[1,1]],alpha=0.6)
     plt.grid(axis='y')
     plt.ylabel('sum of '+y_label)
     plt.legend()
